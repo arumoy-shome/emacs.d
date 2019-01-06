3384d1abeaf3e23b5a98de9616de124c0f9d9167
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-01-06 15:12:54 +0100

    January 6, 2018: Configuration Bankruptcy
    initial commit.

---
+++ init.el
@@ -0,0 +1,11 @@
+(setq ring-bell-function 'ignore
+      x-gtk-use-system-tooltips nil
+      use-dialog-box nil)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode 0)
+
+(desktop-save-mode t)
+(save-place-mode t)
