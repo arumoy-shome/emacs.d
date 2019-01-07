7a6e992b80fd79f510291c9a35ede65533e34e96
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-01-07 07:27:10 +0100

    extract straight.el & use-package into separate file

--- elisp/core.el
+++ elisp/core.el
@@ -21,25 +21,5 @@
 (setq gnutls-verify-error t
       tls-checktrust t)
 
-;; init straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-;; straight.el config
-(setq straight-use-package-by-default t)
-
-;; init use-package we have to use straight-use-package since
-;; use-package is not loaded yet
-(straight-use-package 'use-package)
 
 (provide 'core)
