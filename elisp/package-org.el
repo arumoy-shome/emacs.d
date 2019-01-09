2360313bffd70ebad136e0e386b250604776b153
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-01-09 02:44:01 +0100

    elisp/package-org: template for notes
    Add a template for capturing notes. The CUSTOM_ID property is auto
    filled with a unique identifier (check format-time-string) to
    understand the expansion.

--- elisp/package-org.el
+++ elisp/package-org.el
@@ -1,3 +1,9 @@
+(defvar aru/org-todo-file "~/org/todo.org"
+  "File to use for capturing org Todo items")
+
+(defvar aru/org-note-file "~/org/notes.org"
+  "File to use for capturing org Note items")
+
 (defun aru/setup-org-agenda ()
   "Setup the org agenda mode. This function is run when the
 org-agenda-finalize-hook is run"
@@ -8,7 +14,10 @@ org-agenda-finalize-hook is run"
 loaded"
   (setq org-default-notes-file (expand-file-name "notes.org" org-directory)
 	org-capture-templates
-	'(("t" "Todo" entry (file+headline "~/org/todo.org" "Inbox")
-	   "* TODO %?\n  %i\n  %a"))))
+	'(("t" "Todo" entry (file+headline aru/org-todo-file "Inbox")
+	   "* TODO %?\n  %i\n  %a"
+	   :prepend t)
+	  ("n" "Note" entry (file+headline aru/org-note-file "Inbox")
+	   "* %?\n  :PROPERTIES:\n  :CUSTOM_ID: %<%Y%m%d%H%M%S>\n  :END:\n  %i\n  %a"))))
 
 (provide 'package-org)
