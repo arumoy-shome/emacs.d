4a36040a1f50af289d7c9806a340563eb8935418
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-01-29 01:21:03 +0100

    package-helm: init aru/helm-browse-bib-notes
    an addition to the note browsing helper functions to quickly browse my
    bibliography notes.

--- elisp/package-helm.el
+++ elisp/package-helm.el
@@ -4,6 +4,9 @@
 (defvar aru/project-notes-dir "~/org/projects/"
   "Directory containing my project notes")
 
+(defvar aru/bib-notes-dir "~/org/bib/"
+  "Directory containing my bibliography notes")
+
 (defun aru/helm-browse-notes ()
     "Open the notes dir in helm-find-files"
   (interactive)
@@ -13,6 +16,11 @@
   "Open the projects notes dir in helm-find-files"
   (interactive)
   (helm-find-files-1 aru/project-notes-dir))
+
+(defun aru/helm-browse-bib-notes ()
+  "Open the bibliography notes dir in helm-find-files"
+  (interactive)
+  (helm-find-files-1 aru/bib-notes-dir))
 
 (provide 'package-helm)
 
