d316d0fbf3e5896b39567899b95935fdef6d5512
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-03-02 11:19:06 +0100

    custom: updates

--- custom.el
+++ custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default))
+	'(custom-safe-themes
+		 '("8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default))
+ '(doc-view-resolution 300)
  '(doom-themes-enable-bold t)
- '(doom-themes-enable-italic t))
+ '(doom-themes-enable-italic t)
+ '(mu4e-maildir "~/mail"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
