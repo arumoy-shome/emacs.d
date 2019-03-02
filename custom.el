cfd70700ae79aed30340a3f5ca084f3e35ee33dd
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-03-02 12:29:05 +0100

    custom: update

--- custom.el
+++ custom.el
@@ -4,11 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
 	'(custom-safe-themes
-		 '("8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default))
+		 '("6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" default))
  '(doc-view-resolution 300)
  '(doom-themes-enable-bold t)
  '(doom-themes-enable-italic t)
- '(mu4e-maildir "~/mail"))
+ '(mu4e-maildir "~/mail" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
