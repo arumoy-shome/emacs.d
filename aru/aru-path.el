75c1a494da7263b37550b7291e3d9ceb5d7eeb19
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-09-13 19:51:22 +0200

    [refactor]: shell related updates
    Use zsh as the default shell (now that I have refactored zsh to play
    nice with emacs). Also update path settings.

--- aru/aru-path.el
+++ aru/aru-path.el
@@ -1,12 +1,10 @@
-;; used by shell and term
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+;; emacs picks up the paths set explicitly in the config file but does
+;; not pick up the paths set by libexec, so we add those back ourselves
 (setenv "PATH" (concat "/Library/TeX/texbin:" (getenv "PATH")))
-(setenv "PATH" (concat "/usr/local/opt/python@3.8/libexec/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "/usr/local/opt/ruby/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "/usr/local/lib/ruby/gems/2.7.0/bin:" (getenv "PATH")))
 
 ;; used by emacs to find programs (such as grep and find)
 (add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/usr/local/opt/coreutils/libexec/gnubin")
 (add-to-list 'exec-path "/Library/TeX/texbin")
 (add-to-list 'exec-path "/usr/local/opt/python@3.8/libexec/bin")
 (add-to-list 'exec-path "/usr/local/opt/ruby/bin")
