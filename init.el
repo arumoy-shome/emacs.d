c50c9414c2e3b202b2ad7aaaa896685f3538de3c
Arumoy Shome <contact@arumoy.me>
2021-06-08 23:14:11 +0200

    [feature] init org-todo-sequence for papers
    I generally follow the 3 pass technique while reading papers. With
    this commit I add 3 new todo sequences: "1PASS", "2PASS" and "3PASS"
    indicating how many passes I have done for a paper.

--- init.el
+++ init.el
@@ -476,7 +476,8 @@ _d_: Diagnostics' buffer
   ;; todo
   (setq org-todo-keywords
 	'((sequence "TODO(t)" "|" "DONE(d!)")
-	  (sequence "NEXT(n)" "WAITING(w@/!)" "LATER(l)" "|" "CANCELLED(c@)")))
+	  (sequence "NEXT(n)" "WAITING(w@/!)" "LATER(l)" "|" "CANCELLED(c@)")
+          (sequence "1PASS(1)" "2PASS(2)" "3PASS(3)")))
   (setq org-todo-keyword-faces
 	'(("WAITING" :inherit default :weight bold)
 	  ("LATER" :inherit warning :weight bold)))
